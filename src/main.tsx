54a8a0ccec58d0b4124889fa9e6224e99e53354b
Alex Ivanov <oleksii120@gmail.com>
2024-07-09 14:56:27 +0300

    Added styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
-import "./index.css"
+import "./css/index.css"
+import "./css/tailwind.css"
 import {BrowserRouter} from "react-router-dom"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
